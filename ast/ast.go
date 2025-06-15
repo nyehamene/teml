c805795be1c6cdd8ecf888bcbaae875626193ddd
nyehamene <nhcdeveloper@gmail.com>
2025-06-15 11:15:30 -0400

    (ast) using

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,14 @@
 package ast
 
-import "github.com/eml-lang/teml/token"
+import (
+	"github.com/eml-lang/teml/assert"
+	"github.com/eml-lang/teml/token"
+)
 
 type File struct {
 	Pkg     Package
 	imports []Import
+	usings  []Using
 }
 
 type Node interface {
@@ -21,6 +25,11 @@ type Import struct {
 	Path  token.Token
 }
 
+type Using struct {
+	idents []token.Token
+	From   token.Token
+}
+
 type IntErrorNode int
 
 const (
@@ -30,9 +39,19 @@ const (
 
 func (Package) node() {}
 func (Import) node()  {}
+func (Using) node()   {}
 
 func (IntErrorNode) node() {}
 
+func (u *Using) addIdent(id token.Token) {
+	assert.Assert(id.Kind == token.Ident, "expected ident")
+	u.idents = append(u.idents, id)
+}
+
 func (f *File) addImport(imp Import) {
 	f.imports = append(f.imports, imp)
+}
+
+func (f *File) addUsing(use Using) {
+	f.usings = append(f.usings, use)
 }
